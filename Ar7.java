62ac7c4b1ea540638e5138ae2ecefec808c20906
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 21:27:31 +0530

    sent Ar7

---
+++ Ar7.java
@@ -0,0 +1,15 @@
+class Ar7{
+	public static void main(String []as){
+		char []gun={'5','i','q','9','/','?','#','$'};
+		System.out.println(gun[1]);
+		System.out.println(gun[02]);
+		System.out.println(gun[0]);
+		System.out.println(gun[03]);
+		System.out.println(gun[04]);
+		System.out.println(gun[05]);
+		System.out.println(gun[06]);
+		System.out.println(gun[07]);
+
+	}
+
+}																			
